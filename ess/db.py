0f822273ef94072aef3ed240664558738ffe1ae0
Sven Haardiek <Sven@Haardiek.de>
2014-05-09 15:41:09 +0200

    Worked on db.py
    Changed backrefs
    Changed music_song.name in music_song.title
    Added class Player, Playlist
    
    Signed-off-by: Sven Haardiek <Sven@Haardiek.de>

--- ess/db.py
+++ ess/db.py
@@ -10,10 +10,10 @@ sys.setdefaultencoding('utf8')
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
 from sqlalchemy.orm import sessionmaker, relationship, backref
-
+from sqlalchemy.schema import PrimaryKeyConstraint
 Base = declarative_base()
 
-# init
+# Init
 def get_session():
 	DATABASE = 'mysql://ess:secret@localhost/ess?charset=utf8'
 	engine = create_engine(DATABASE)
@@ -22,10 +22,7 @@ def get_session():
 	return Session()
 
 
-#############################################################################
-## Database Schema Definition                                              ##
-#############################################################################
-
+# Database Schema Definition
 class Artist(Base):
 	__tablename__ = 'music_artist'
 
@@ -45,7 +42,7 @@ class Album(Base):
 	name      = Column('name', String(255), nullable=False)
 	artist_id = Column('artist_id', None, ForeignKey('music_artist.id'))
 
-	artist = relationship("Artist", backref=backref('album', order_by=name))
+	artist    = relationship("Artist", backref=backref('music_albums'))
 
 	def __repr__(self):
 		return '<Album(id=%i, name="%s", artist_id=%i)>' % \
@@ -55,8 +52,9 @@ class Album(Base):
 class Song(Base):
 	__tablename__ = 'music_song'
 
-	id           = Column('id', Integer(unsigned=True), primary_key=True, autoincrement=True)
-	title        = Column('name', String(255), nullable=False)
+	id           = Column('id', Integer(unsigned=True), primary_key=True,
+			autoincrement=True)
+	title        = Column('title', String(255), nullable=False)
 	date         = Column('date', String(255))
 	tracknumber  = Column('tracknumber', Integer(unsigned=True))
 	times_played = Column('times_played', Integer(unsigned=True))
@@ -65,10 +63,37 @@ class Song(Base):
 	artist_id    = Column('artist_id', None, ForeignKey('music_artist.id'))
 	album_id     = Column('album_id', None, ForeignKey('music_album.id'))
 
-	artist = relationship("Artist", backref=backref('song', order_by=title))
-	album  = relationship("Album",  backref=backref('song', order_by=title))
+	artist       = relationship("Artist", backref=backref('music_songs'))
+	album        = relationship("Album",  backref=backref('music_songs'))
 
 	def __repr__(self):
 		return '<Song(id=%i, title="%s", artist_id=%i)>' % \
 			(self.id, self.title, self.artist_id)
+
+class Player(Base):
+	__tablename__ = 'player'
+
+	playername  = Column('playername', String(255), primary_key=True)
+	description = Column('description', String(255))
+	song_id     = Column('song_id', None, ForeignKey('music_song.id'))
+
+	current     = relationship("Song", backref=backref('players'))
+
+	def __repr__(self):
+		return '<Player(playername=%s, description="%s", song_id=%i)>' % \
+			(self.playername, self.description, self.song_id)
+
+class Playlist(Base):
+	__tablename__ = 'playlist'
+
+	order      = Column('order', Integer(unsigned=True), primary_key=True)
+	playername = Column('playername', None, ForeignKey('player.playername'),
+			primary_key=True)
+	song_id    = Column('song_id', None, ForeignKey('music_song.id'))
+
+	song       = relationship("Song",  backref=backref('playlists'))
+
+	def __repr__(self):
+		return '<(playername=%s, order="%i", song_id=%i)>' % \
+			(self.playername, self.order, self.song_id)
 
