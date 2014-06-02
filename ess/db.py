0a9552dda2042e10a78781f707f75a332361518c
Lars Kiesow <lkiesow@uos.de>
2014-06-02 20:34:13 +0200

    Changed some names in database definition
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- ess/db.py
+++ ess/db.py
@@ -8,9 +8,9 @@ sys.setdefaultencoding('utf8')
 
 from ess import config
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import sessionmaker, relationship, backref
-from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
+from sqlalchemy.schema import ForeignKeyConstraint
 Base = declarative_base()
 
 # Init
@@ -29,7 +29,7 @@ def get_session():
 
 # Database Schema Definition
 class Artist(Base):
-	__tablename__ = 'music_artist'
+	__tablename__ = 'artist'
 
 	id   = Column('id', Integer(unsigned=True), autoincrement='ignore_fk',
 			primary_key=True)
@@ -38,9 +38,12 @@ class Artist(Base):
 	def __repr__(self):
 		return '<Artist(id=%i, name="%s")>' % (self.id, self.name)
 
+	def serialize(self, expand=0):
+		return {'id':self.id, 'name':self.name}
+
 
 class Album(Base):
-	__tablename__ = 'music_album'
+	__tablename__ = 'album'
 
 	id        = Column('id', Integer, autoincrement='ignore_fk',
 			primary_key=True)
@@ -54,8 +57,15 @@ class Album(Base):
 				(self.id, self.name, self.artist_id)
 
 
-class Song(Base):
-	__tablename__ = 'music_song'
+	def serialize(self, expand=0):
+		if expand:
+			return {'id':self.id, 'name':self.name,
+					'artist':self.artist.serialize(expand-1)}
+		return {'id':self.id, 'name':self.name, 'artist':self.artist_id}
+
+
+class Media(Base):
+	__tablename__ = 'media'
 
 	id           = Column('id', Integer(unsigned=True), primary_key=True,
 			autoincrement=True)
@@ -63,8 +73,9 @@ class Song(Base):
 	date         = Column('date', String(255))
 	tracknumber  = Column('tracknumber', Integer(unsigned=True))
 	times_played = Column('times_played', Integer(unsigned=True))
-	uri          = Column('uri', String(2**16), nullable=False)
+	path         = Column('path', String(2**16), nullable=False)
 	genre        = Column('genre', String(255))
+	duration     = Column('duration', Integer(unsigned=True), nullable=True)
 	artist_id    = Column('artist_id', ForeignKey('music_artist.id'))
 	album_id     = Column('album_id', ForeignKey('music_album.id'))
 
@@ -75,8 +86,22 @@ class Song(Base):
 		return '<Song(id=%i, title="%s", artist_id=%i)>' % \
 			(self.id, self.title, self.artist_id)
 
+
 	def serialize(self, expand=0):
-		return {k:v for k,v in self.__dict__.items() if not k.startswith('_')}
+		result = {
+				'title'        : self.title,
+				'date'         : self.date,
+				'tracknumber'  : self.tracknumber,
+				'times_played' : self.times_played,
+				'path'         : self.path,
+				'genre'        : self.genre,
+				'duration'     : self.duration,
+				'artist'       : self.artist_id,
+				'album'        : self.album_id}
+		if expand:
+			result['artist'] = self.artist.serialize(expand - 1)
+			result['album']  = self.album.serialize(expand - 1)
+		return result
 
 
 class Player(Base):
@@ -86,12 +111,13 @@ class Player(Base):
 	description = Column('description', String(255))
 	current_idx = Column('current', Integer(unsigned=True), default=None)
 
-	current     = relationship("Playlist", foreign_keys=[playername, current_idx])
+	current     = relationship("PlaylistEntry",
+			foreign_keys=[playername, current_idx])
 
 	__table_args__ = (
 			ForeignKeyConstraint(
 				[playername, current_idx],
-				['playlist.playername', 'playlist.order'],
+				['playlist_entry.playername', 'playlist_entry.order'],
 				use_alter=True,
 				name='fk_current_song'),
 			{})
@@ -100,32 +126,33 @@ class Player(Base):
 		return '<Player(playername=%s, description=%s, current_idx=%i)>' % \
 			(self.playername, self.description, self.current_idx)
 
+
 	def serialize(self, expand=0):
-		if expand:
-			return {'playername':self.playername, 'description':self.description,
-					'current_idx':self.current_idx, 'current':self.current.serialize(expand-1)}
-		return {'playername':self.playername, 'description':self.description,
-				'current_idx':self.current_idx}
+		return {
+				'playername'  : self.playername,
+				'description' : self.description,
+				'current'     : self.current.serialize(expand-1) \
+						if expand else self.current_idx }
 
 
-class Playlist(Base):
-	__tablename__ = 'playlist'
+class PlaylistEntry(Base):
+	__tablename__ = 'playlist_entry'
 
 	playername  = Column('playername', ForeignKey('player.playername'), primary_key=True)
 	order       = Column('order', Integer(unsigned=True), primary_key=True, autoincrement=False)
-	song_id     = Column('song_id', ForeignKey('music_song.id'), nullable=False)
+	media_id    = Column('media_id', ForeignKey('media.id'), nullable=False)
 
-	song        = relationship("Song",  backref=backref('playlist'))
+	media       = relationship("Media",  backref=backref('playlist_entry'))
 	player      = relationship("Player", foreign_keys=[playername])
 
 	def __repr__(self):
-		return '<(playername=%s, order=%i, song_id=%i)>' % \
-			(self.playername, self.order, self.song_id)
+		return '<(playername=%s, order=%i, media_id=%i)>' % \
+			(self.playername, self.order, self.media_id)
+
 
 	def serialize(self, expand=0):
 		if expand:
-			return {'playername':self.playername,
-					'player':self.player.serialize(expand-1), 'order':self.order,
-					'song_id':self.song_id, 'song':self.song.serialize(expand-1)}
-		return {'playername':self.playername, 'order':self.order,
-				'song_id':self.song_id}
+			return {
+					'player' : self.player.serialize(expand-1) if expand else self.playername,
+					'order'  : self.order,
+					'media'  : self.media.serialize(expand-1) if expand else self.media_id}
