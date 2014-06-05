48b4a55debb3401cbe5ed2568742b7992e9785af
Lars Kiesow <lkiesow@uos.de>
2014-06-05 13:29:51 +0200

    Added PlaylistUI React.js components
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- ess/static/playlistui.js
+++ ess/static/playlistui.js
@@ -1,39 +1,38 @@
 /** @jsx React.DOM */
 
-var Result = React.createClass({
+var Entry = React.createClass({
 	render: function() {
 		return (
 			<tr>
 				<td>{this.props.artist}</td>
 				<td>{this.props.album}</td>
 				<td>{this.props.title}</td>
-				<td><a href="#" title="Add to playlist">⏩</a></td>
 			</tr>
 			);
 	}
 });
 
-var SearchResult = React.createClass({
+var Playlist = React.createClass({
 	render: function() {
 		var resultNodes = this.props.media.map(function (media, index) {
-			return <Result key={index} artist={media.artist.name}
+			return <Entry key={index} artist={media.artist.name}
 			album={media.album.name} title={media.title} />
 		});
 		return <table className="body">{resultNodes}</table>;
 	}
 });
 
-var SearchHeader = React.createClass({
-	handleSearch: function() {
+var PlaylistHeader = React.createClass({
+	handlePlaylist: function() {
 		var keyword = this.refs.keyword.getDOMNode().value.trim();
-		this.props.onSearch(keyword);
+		this.props.onPlaylist(keyword);
 		return false;
 	},
 	render: function() {
 		return (
 			<div className="header search">
-				<form className="searchForm" onSubmit={this.handleSearch}>
-					<input type="text" placeholder="Search…" ref="keyword" />
+				<form className="searchForm" onSubmit={this.handlePlaylist}>
+					<input type="text" placeholder="Playername…" ref="keyword" />
 					<input type="image" src="/static/icons/iconmonstr-magnifier-icon-48.png" alt="Submit" />
 				</form>
 			</div>
@@ -41,28 +40,27 @@ var SearchHeader = React.createClass({
 	}
 });
 
-var SearchUI = React.createClass({
-	handleSearch: function(keyword) {
-		var request = JSON.stringify({ search : keyword });
+var PlaylistUI = React.createClass({
+	handlePlaylist: function(playername) {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			dataType: 'json',
-			headers: { 'Content-Type' : 'application/json' },
-			url: this.props.url,
-			data: request,
+			url: '/playlist/' + playername + '?expand=1',
 			success: function( result ) {
-				this.setState(result);
+				playlist = result[playername];
+				this.setState(playlist);
 			}.bind(this)
 		});
 	 },
 	 getInitialState: function() {
-		 return {media: []};
+		 handlePlaylist('player01');
+		 return [];
 	 },
 	 render: function() {
 		 return (
 			 <div>
-				 <SearchHeader onSearch={this.handleSearch} />
-				 <SearchResult media={this.state.media} />
+				 <PlaylistHeader onSearch={this.handleSearch} />
+				 <Playlist playlist={this.state} />
 			 </div>
 		 );
 	 }
@@ -70,6 +68,6 @@ var SearchUI = React.createClass({
 
 
 React.renderComponent(
-	<SearchUI url="/search" />,
-	document.getElementById('searchui')
+	<PlaylistUI url="/playlist/" />,
+	document.getElementById('playlistui')
 );
